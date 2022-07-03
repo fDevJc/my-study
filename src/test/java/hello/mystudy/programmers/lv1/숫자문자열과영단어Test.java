b13dc791f8be0d0a4b2859c3b058f1fca03e188c
fDevJc <fdevjc@gmail.com>
2022-07-03 20:51:28 +0900

    2021카카오 채용연계형 인턴십 숫자 문자열과 영단어

---
+++ src/test/java/hello/mystudy/programmers/lv1/숫자문자열과영단어Test.java
@@ -0,0 +1,39 @@
+package hello.mystudy.programmers.lv1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class 숫자문자열과영단어Test {
+    숫자문자열과영단어 test = new 숫자문자열과영단어();
+
+    @Test
+    void ex1() {
+        String s = "one4seveneight";
+        int result = test.solution(s);
+        Assertions.assertThat(result).isEqualTo(1478);
+    }
+
+    @Test
+    void ex2() {
+        String s = "23four5six7";
+        int result = test.solution(s);
+        Assertions.assertThat(result).isEqualTo(234567);
+    }
+
+    @Test
+    void ex3() {
+        String s = "2three45sixseven";
+        int result = test.solution(s);
+        Assertions.assertThat(result).isEqualTo(234567);
+    }
+
+    @Test
+    void ex4() {
+        String s = "123";
+        int result = test.solution(s);
+        Assertions.assertThat(result).isEqualTo(123);
+    }
+
+}
