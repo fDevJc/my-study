8b65d454b4b10ff0fbb48fe481b30eebdf28e761
fDevJc <fdevjc@gmail.com>
2022-07-30 19:35:44 +0900

    프로그래머스 위장

---
+++ src/test/java/hello/mystudy/programmers/lv1/위장Test.java
@@ -0,0 +1,23 @@
+package hello.mystudy.programmers.lv1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class 위장Test {
+    위장 test = new 위장();
+
+    @Test
+    void ex1() {
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        int result = test.solution(clothes);
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void ex2() {
+        String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+        int result = test.solution(clothes);
+        assertThat(result).isEqualTo(3);
+    }
+}
